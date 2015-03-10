0450354e651359eaac5fa243e76fa8061d8fbb14
ohendriks <hendriks@robot-rose.nl>
2015-03-10 15:06:48 +0100

    Increase min drive vel

--- rose_arc_local_planner/src/arc_local_planner.cpp
+++ rose_arc_local_planner/src/arc_local_planner.cpp
@@ -11,7 +11,7 @@ namespace rose_navigation{
 #define MAX_VEL_THETA_INPLACE	0.4
 
 #define MIN_VEL_ABS 			0.075
-#define MIN_VEL_ABS_DRIVE 		0.1
+#define MIN_VEL_ABS_DRIVE 		0.2
 #define MIN_VEL_THETA 			0.05
 #define MIN_VEL_THETA_INPLACE 	0.15
 #define MAX_ACC_X 				0.4
@@ -772,8 +772,6 @@ bool ArcLocalPlanner::findBestCommandVelocity(const vector<PoseStamped>& plan, T
 			best_trajectory 		= trajectory;
 		}
 	}
-
-	
 
 	if(not best_trajectory.trajectory.empty())
 		best_cmd_vel = best_trajectory.velocity;
