20237bd81b9602fee9a85562f5e41b1242f693b9
ohendriks <hendriks@robot-rose.nl>
2015-04-22 21:42:01 +0200

    Velocity sampeling range tune-ing.

--- rose_arc_local_planner/src/arc_local_planner.cpp
+++ rose_arc_local_planner/src/arc_local_planner.cpp
@@ -569,11 +569,11 @@ bool ArcLocalPlanner::findBestCommandVelocity(const vector<PoseStamped>& plan, T
 
     float current_radius  = currentRadius();
 
-    int num_tang_velocities         = 4;
+    int num_tang_velocities         = 5;
     int num_rot_velocities          = 15;
     int num_dts                     = 4;
 
-    float stepsize_tang_velocities  = 0.05;
+    float stepsize_tang_velocities  = 0.04;
     float stepsize_rot_velocities   = 0.0675;
     float stepsize_dts              = 0.3;
 
